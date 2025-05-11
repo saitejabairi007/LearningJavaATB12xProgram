f0643e06810c253e41e2040e9ecd44f2c5753086
sai teja <saitejabairi007@gmail.com>
2025-05-11 13:59:50 +0530

    First commit

--- src/Java_Basics/EX02_comments.java
+++ src/Java_Basics/EX02_comments.java
@@ -4,7 +4,9 @@ public class EX02_comments {
     public static void main(String[] args) {
         //single line comments
         /* this is multiple lines comment
-        System.out.println("comments");*/
+        System.out.println("comments");
+
+        */
 
     }
 }
