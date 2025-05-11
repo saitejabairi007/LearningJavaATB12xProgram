626537d2e7118068f52f41928502d16b5af47ecb
sai teja <saitejabairi007@gmail.com>
2025-05-11 13:58:55 +0530

    First commit

--- src/Java_Basics/EX01_HelloJava.java
+++ src/Java_Basics/EX01_HelloJava.java
@@ -1,4 +1,8 @@
 package Java_Basics;
 
 public class EX01_HelloJava {
+    public static void main(String[] args) {
+        System.out.println("hello Java");//This is my statement
+        System.out.println("This is my first task");
+    }
 }
---
+++ src/Java_Basics/EX02_comments.java
@@ -0,0 +1,10 @@
+package Java_Basics;
+
+public class EX02_comments {
+    public static void main(String[] args) {
+        //single line comments
+        /* this is multiple lines comment
+        System.out.println("comments");*/
+
+    }
+}
